b04fc494e0277a61e43b73d41c2864dcb5154b53
tommyixi <Carpenito.t@gmail.com>
2016-02-04 18:55:24 -0800

    Fixes callback uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['key']
   config.secret = ENV['secret']
-  config.redirect_uri = "http://shopify-rain.herokuapp.com/shopify/callback"
+  config.redirect_uri = "https://rainify.herokuapp.com/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
