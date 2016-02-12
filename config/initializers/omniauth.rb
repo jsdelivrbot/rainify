24fa26a31733abff0b099d813e9db2ca4ea2b5b9
Tommyixi <carpenito.t@gmail.com>
2016-02-12 11:01:52 -0800

    Adds redirect uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
-    redirect_uri: ShopifyApp.configuration.redirect_uri,
+    redirect_uri: "http://rainify.herokuapp.com/auth/shopify/callback",
     scope: ShopifyApp.configuration.scope
 end
