c0bfe7772e3178d8c866eaab626b72f5b622bd99
Tommyixi <carpenito.t@gmail.com>
2016-02-11 18:42:43 -0800

    prepares for heroku deployment

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['key']
   config.secret = ENV['secret']
-  config.redirect_uri = "localhost:3000/shopify/callback"
+  config.redirect_uri = "rainify.herokuapp.com/shopify/callback"
   config.scope = "write_script_tags, read_script_tags"
   config.embedded_app = true
 end
