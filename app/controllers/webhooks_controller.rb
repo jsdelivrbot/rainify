1a9e11bd92c973340faa55ab279e8fba46316b2f
tommyixi <Carpenito.t@gmail.com>
2017-03-04 23:42:41 -0800

    Updates webhook params

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,7 +1,7 @@
 class WebhooksController < AuthenticatedController
 
   def uninstall
-    shop = Shop.where(shopify_domain: params[:shopify_domain])
+    shop = Shop.where(shopify_domain: params[:domain])
     shop.update_attributes(uninstall: true)
   end  
   
