fb2767b38dcd41140017a298e9ccdc263cdc4537
tommyixi <Carpenito.t@gmail.com>
2018-02-10 10:06:35 -0800

    Fixes bugs with subscriiption

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,11 +4,10 @@ class PaymentsController < AuthenticatedController
 
 	def show
 		payment = ShopifyAPI::RecurringApplicationCharge.find(params[:charge_id])
-		payment.activate
-		if current_shop.shopify_domain == "rainboots-and-shoes.myshopify.com"
-			current_shop.update_attributes(charge: params[:charge_id], flag: true)
-		else
-			current_shop.update_attributes(charge: params[:charge_id])
+		if payment.status == "accepted" 
+			payment.activate
+		end
+		current_shop.update_attributes(charge: params[:charge_id])
 		end
 		redirect_to "/"
 	end
@@ -18,13 +17,13 @@ class PaymentsController < AuthenticatedController
 		payment.name = "Monthly Rainify Charge"
 		payment.price = "1.99"
 		payment.return_url = "http:\/\/rainify.herokuapp.com/payments/activate"
-		payment.save
-		new_script = ShopifyAPI::ScriptTag.new
-  	new_script.event = "onload"
-  	new_script.src = "https://rawgit.com/Tommyixi/rainify/master/public/rainify.js"
-  	new_script.save
-
-		redirect_to payment.confirmation_url
+		if payment.save
+			new_script = ShopifyAPI::ScriptTag.new
+  		new_script.event = "onload"
+  		new_script.src = "https://rawgit.com/Tommyixi/rainify/master/public/rainify.js"
+  		new_script.save
+			redirect_to payment.confirmation_url
+		end
 	end
 end
 
